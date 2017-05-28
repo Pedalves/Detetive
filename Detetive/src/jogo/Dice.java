957f60e2dc69f89265ffb2550c69938dee80b42f
Felipe Pessoa <felipessoaf@gmail.com>
2017-05-28 00:05:52 -0300

    Dado rolando

---
+++ Detetive/src/jogo/Dice.java
@@ -0,0 +1,88 @@
+package jogo;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.ThreadLocalRandom;
+
+import javax.imageio.ImageIO;
+
+import com.sun.xml.internal.ws.api.streaming.XMLStreamReaderFactory.Default;
+
+public class Dice 
+{
+	public boolean PaintDice = false;
+	public BufferedImage DiceImage1;
+	public BufferedImage DiceImage2;
+	
+	private BufferedImage[] diceImages;
+	
+	public Dice()
+	{
+		diceImages = new BufferedImage[6];
+		
+		try
+		{
+			for(int i = 0; i < 6; i++)
+			{
+				diceImages[i] = ImageIO.read(new File("Images\\dado" + Integer.toString(i+1) + ".png"));
+			}
+		}
+		catch(IOException e)
+		{
+			System.out.println("ERRO ao carregar imagem do dado");
+		}	
+	}
+	
+	public int RollDice()
+	{
+		PaintDice = true;
+		int num1 = ThreadLocalRandom.current().nextInt(1, 6 + 1);
+		int num2 = ThreadLocalRandom.current().nextInt(1, 6 + 1);
+		switch (num1){
+			case 1:
+				DiceImage1 = diceImages[0];
+				break;
+			case 2:
+				DiceImage1 = diceImages[1];
+				break;
+			case 3:
+				DiceImage1 = diceImages[2];
+				break;
+			case 4:
+				DiceImage1 = diceImages[3];
+				break;
+			case 5:
+				DiceImage1 = diceImages[4];
+				break;
+			case 6:
+				DiceImage1 = diceImages[5];
+				break;
+			default:
+				break;
+		}
+		switch (num2){
+		case 1:
+			DiceImage2 = diceImages[0];
+			break;
+		case 2:
+			DiceImage2 = diceImages[1];
+			break;
+		case 3:
+			DiceImage2 = diceImages[2];
+			break;
+		case 4:
+			DiceImage2 = diceImages[3];
+			break;
+		case 5:
+			DiceImage2 = diceImages[4];
+			break;
+		case 6:
+			DiceImage2 = diceImages[5];
+			break;
+		default:
+			break;
+	}
+		return num1 + num2; 
+	}
+}
--- Detetive/src/views/GameView.java
+++ Detetive/src/views/GameView.java
@@ -1,12 +1,15 @@
 package views;
 
+import java.awt.Component;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
+import javax.swing.JButton;
 
+import jogo.Dice;
 import jogo.Player;
 
 
@@ -16,6 +19,10 @@ public class GameView extends View
 	private BufferedImage bgImage;
 	private String gameFile;
 	
+	private boolean paintDice = false;
+	private Dice dice;
+	private BufferedImage diceImage;
+	
 	private Player player1;
 	private Player player2;
 	private Player player3;
@@ -23,24 +30,6 @@ public class GameView extends View
 	private Player player5;
 	private Player player6;
 	       	
-	@Override
-	protected void paintComponent(Graphics g)
-	{
-	    super.paintComponent(g);
-	    g.drawImage(bgImage, 0, 0, null);
-	    
-	    //Players
-	    if(player1 != null)
-	    {
-		    g.drawImage(player1.pawnImage, player1.PosX, player1.PosY, null);
-		    g.drawImage(player2.pawnImage, player2.PosX, player2.PosY, null);
-		    g.drawImage(player3.pawnImage, player3.PosX, player3.PosY, null);
-		    g.drawImage(player4.pawnImage, player4.PosX, player4.PosY, null);
-		    g.drawImage(player5.pawnImage, player5.PosX, player5.PosY, null);
-		    g.drawImage(player6.pawnImage, player6.PosX, player6.PosY, null);
-	    }
-	}
-	
 	public GameView()
 	{
 		super();
@@ -51,6 +40,8 @@ public class GameView extends View
 		player4 = new Player("Plum", 630, 525);
 		player5 = new Player("Scarlet", 230, 652);
 		player6 = new Player("White", 280, 50);
+		
+		dice = new Dice();
 		
 		try
 		{
@@ -68,10 +59,42 @@ public class GameView extends View
 
 		this.gameFile = gameFile;		
 	}
+	
+	@Override
+	protected void paintComponent(Graphics g)
+	{
+	    super.paintComponent(g);
+	    g.drawImage(bgImage, 0, 0, null);
+	    
+	    //Players
+	    if(player1 != null)
+	    {
+		    g.drawImage(player1.pawnImage, player1.PosX, player1.PosY, null);
+		    g.drawImage(player2.pawnImage, player2.PosX, player2.PosY, null);
+		    g.drawImage(player3.pawnImage, player3.PosX, player3.PosY, null);
+		    g.drawImage(player4.pawnImage, player4.PosX, player4.PosY, null);
+		    g.drawImage(player5.pawnImage, player5.PosX, player5.PosY, null);
+		    g.drawImage(player6.pawnImage, player6.PosX, player6.PosY, null);
+	    }
+	    
+	    if(dice.PaintDice)
+	    {
+	    	g.drawImage(dice.DiceImage1, 700, 200, null);
+	    	g.drawImage(dice.DiceImage2, 800, 200, null);
+	    	paintDice = false;
+	    }
+	}
 
 	@Override
 	public void setupUI() 
 	{
+		JButton diceButton = new JButton("Rolar dado");
+		diceButton.setAlignmentX(Component.CENTER_ALIGNMENT);		
+		diceButton.addActionListener(e -> {
+			dice.RollDice();
+			repaint();
+		});
 		
+		add(diceButton);
 	}
 }
